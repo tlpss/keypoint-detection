add integration test to compare numerical results and fix logging dir

test/test_detector.py
@@ -12,8 +12,6 @@ from keypoint_detection.models.metrics import KeypointAPMetric
 from keypoint_detection.train.utils import create_pl_trainer
 from keypoint_detection.utils.heatmap import generate_keypoints_heatmap
 
-from .configuration import DEFAULT_HPARAMS
-
 
 class TestHeatmapUtils(unittest.TestCase):
     def setUp(self) -> None:
@@ -104,4 +102,15 @@ class TestModel(unittest.TestCase):
         self.assertTrue(torch.var(heatmap).item() < 0.1)
 
 
-# TODO: test model train script
+if __name__ == "__main__":
+    import os
+
+    command = (
+        "python keypoint_detection/train/train.py --keypoint_channels 'corner_keypoints flap_corner_keypoints'"
+        "--keypoint_channel_max_keypoints '-1 -1'"
+        "--image_dataset_path test/test_dataset"
+        "--json_dataset_path test/test_dataset/dataset.json"
+        " --batch_size  1"
+    )
+    print(command)
+    os.system(command)