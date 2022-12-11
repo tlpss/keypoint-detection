d6a8eb9ec94d688b34b23d9c0c4d779bf27bb3e3
tlpss <thomas17.lips@gmail.com>
2022-12-11 14:48:50 +0100

    add warning about trailing spaces

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -2,6 +2,7 @@
 # This is an integration test for the keypoint detector
 # Run from the repo's root folder using bash test/integration_test.sh
 
+# make sure to remove all trailing spaces from the command, as this would result in an error when using bash.
 python keypoint_detection/train/train.py \
 --keypoint_channel_configuration  "box_corner0= box_corner1 = box_corner2= box_corner3; flap_corner0 ; flap_corner2" \
 --json_dataset_path "test/test_dataset/coco_dataset.json" --batch_size  2 --wandb_project "keypoint-detector-integration-test" \
