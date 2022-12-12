55992d5919d42ea8954265610996931c1364aeed
Lucas Van Dijck <lucas.pa.vd@gmail.com>
2022-12-12 17:52:24 +0100

    changed default max_keypoints to 20

--- keypoint_detection/models/detector.py
+++ keypoint_detection/models/detector.py
@@ -72,7 +72,7 @@ class KeypointDetector(pl.LightningModule):
         )
         parser.add_argument(
             "--max_keypoints",
-            default=-1,
+            default=20,
             type=int,
             help="the maximum number of keypoints to predict from the generated heatmaps. If set to -1, skimage will look for all peaks in the heatmap, if set to N (N>0) it will return the N most most certain ones.",
         )
